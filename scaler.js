c1c664894d07003818eb7e6375609b2828f92816
Roque Francisco <roquefsd@gmail.com>
2021-07-19 18:10:17 -0300

    parameters

--- scaler.js
+++ scaler.js
@@ -1,4 +1,4 @@
-let size = 2;
+let size = 4;
 let canvas = document.createElement('canvas');
 let ctx = canvas.getContext('2d');
 let canvas_resize = document.createElement('canvas');
@@ -10,6 +10,7 @@ let ctx3 = canvas3.getContext('2d');
 let image_data;
 let mapped = {};
 var capturer = new CCapture({ format: 'webm', framerate: 60, motionBlurFrames: 1 });
+let padding = 1;
 
 let img = new Image();
 img.src = 'dk.jpg';
@@ -18,14 +19,15 @@ img.onload = () => {
     canvas.height = img.height;
     canvas_resize.width = img.width / (size / 2);
     canvas_resize.height = img.height / (size / 2);
-    canvas2.width = img.width;
-    canvas2.height = img.height;
-    canvas3.width = img.width;
-    canvas3.height = img.height;
+
+    canvas2.width = img.width + (canvas_resize.width * padding);
+    canvas2.height = img.height + (canvas_resize.height * padding);
+    canvas3.width = img.width + (canvas_resize.width * padding);
+    canvas3.height = img.height + (canvas_resize.height * padding);
     ctx.drawImage(img, 0, 0);
     ctx_resize.drawImage(img, 0, 0, canvas_resize.width, canvas_resize.height);
     // ctx2.fillStyle = '#000';
-    ctx2.fillRect(0, 0, canvas.width, canvas.height)
+    ctx2.fillRect(0, 0, canvas2.width, canvas2.height)
 
     // document.body.appendChild(canvas);
     // document.body.appendChild(canvas_resize);
@@ -40,7 +42,6 @@ img.onload = () => {
 }
 
 let rendering = false;
-
 function mapRays() {
     let data = image_data.data;
     for (let y = 0; y < canvas_resize.height; ++y) {
@@ -53,7 +54,7 @@ function mapRays() {
 
             mapped[y][x] = {
                 x: (x * (size / 2)) + (Math.random() / 2),
-                y: (y * (size / 2)) + (Math.random() / 2),
+                y: (y * (size / 2)) + (Math.random() / 4),
                 rx: radius * 1.9,
                 ry: radius * .1,
                 radius,
@@ -69,29 +70,35 @@ function mapRays() {
 }
 
 let steps = 8;
-let filter = `contrast(1.1) brightness(${1.4}) saturate(1.4) blur(${size / 2}px)`;
+let count = 0;
+let filter = `contrast(1.1) brightness(${1.4}) saturate(1.4) blur(${size / 4}px)`;
 function draw(y) {
     if (y >= canvas_resize.height) {
         y = 0;
-
+        
+        return;
+        
         if (rendering === false) {
             rendering = true;
             console.time('render');
             capturer.start();
         }
-        else if (rendering === true) {
+        else if (rendering === true && count > 1) {
             console.timeEnd('render');
             capturer.stop();
             capturer.save();
             rendering = null
         }
+        console.log(rendering, count);
+        
+        count++;
     }
-
+    
     requestAnimationFrame(() => { draw(y + 1) });
     // ctx3.drawImage(canvas, 0, 0);
     ctx3.drawImage(canvas2, 0, 0);
     capturer.capture(canvas3);
-
+    
     let w = canvas_resize.width;
     let h = canvas_resize.height;
 
@@ -100,19 +107,20 @@ function draw(y) {
         let ray = mapped[y][x];
         ctx2.beginPath();
         ctx.moveTo(ray.x, ray.y);
-        ctx2.ellipse(ray.x, ray.y, ray.rx, ray.ry, 0, 0, 2 * Math.PI, true);
+        ctx2.ellipse(ray.x + (x * (padding * 1.1)), ray.y + (y * padding), ray.rx, ray.ry, 0, 0, 2 * Math.PI, true);
         ctx2.fillStyle = ray.color;
+        ctx2.fill();
         ctx2.fill();
         // }
     }
-
+    
+    ctx2.filter = filter;
     if (y % Math.round(canvas.height / steps) === 0) {
-        ctx2.filter = filter;
     }
 
     if (y % Math.round(canvas.height / (steps * 4)) === 0) {
         ctx2.fillStyle = `rgba(0, 0, 0, ${1 / (steps * 6)})`;
-        ctx2.fillRect(0, 0, canvas.width, canvas.height);
+        // ctx2.fillRect(0, 0, canvas.width, canvas.height);
     }
 
 }
